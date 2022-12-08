ebbb00ec7ae248eed5dbfc845fde2041f3933c05
Meko Steinberg <steinberg.meko@outlook.com>
2022-12-08 10:06:51 -0800

    Buttons added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Container } from '@mui/system';
-import { AppBar, Paper, Typography } from '@mui/material';
+import { AppBar, Box, Button, Paper, Typography } from '@mui/material';
 
 export default function App() {
+  //initial blog setup for useState
+  const [rant, setRant] = useState([])
+  const [addRant, setAddRant] = useState([])
+
+  //-----------------
+  // NAV Buttons
+  //-----------------
+  const [isRantsVisible, setIsRantsVisible] = useState(true)
+  const [isAddARantVisible, setIsAddARantVisible] = useState(false)
+
+  const showRants = () => {
+    setIsRantsVisible(true);
+    setIsAddARantVisible(false)
+
+  }
+  const showAddARant = () => {
+    setIsRantsVisible(false);
+    setIsAddARantVisible(true)
+
+  }
+
   return (
     <Container>
       {/* //----Header----// */}
       <Paper>
-        <AppBar position="static" sx={{ m: 2, ml: 0, p: 2, bgcolor: 'blue' }}>
+        <AppBar position="static" sx={{ m: 2, ml: 0, p: 2, bgcolor: 'primary' }}>
           <Typography variant="h3">Blog Rant</Typography>
         </AppBar>
       </Paper>
 
-    </Container> 
+      <Box align="center" sx={{ m: 2, msl: 5 }}>
+        <Button variant={isRantsVisible ? "contained" : "outlined"} color="primary" onClick={showRants} sx={{ mr: 2 }}>Rants</Button>
+        <Button variant={isAddARantVisible ? "contained" : "outlined"} color="primary" onClick={showAddARant} sx={{ mr: 2 }}>Add a Rant</Button>
+      </Box>
+
+
+
+
+
+    </Container>
   )
 }
 
