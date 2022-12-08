a00c3830e49db9047cba9791967c2da2aa9acaea
pmurphy926 <patrick.murphy926@gmail.com>
2022-12-08 13:51:09 -0500

    post and get routes showing data but not desired content

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
-//====================
-//====DEPENDENCIES====
-//====================
+//==============================================
+//===================DEPENDENCIES===============
+//==============================================
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express();
 const Posts = require('./models/post')
+
+//========================================
+//===============ROUTES===================
+//========================================
+app.post('/blog', (req, res) => {
+    Posts.create(req.body, (err, createdPost) => {
+        res.json(createdPost)
+    })
+})
+
+app.get('/blog', (req, res) => {
+    Posts.find({}, (err, foundPost) => {
+        res.json(foundPost)
+    })
+})
 
 //====================
 //======LISTENERS=====
@@ -14,7 +29,7 @@ app.listen(3000, () => {
     console.log('listening')
 })
 
-mongoose.connect('mongodb://localhost:27017/merncrud')
+mongoose.connect('mongodb://localhost:27017/w08d04_lab')
 mongoose.connection.once('open', () => {
     console.log('connected to mongo')
 })
