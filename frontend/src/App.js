2065cdd6404da96122c5f4d484150b917fa33cc1
Meko Steinberg <steinberg.meko@outlook.com>
2022-12-08 10:58:29 -0800

    push front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Container } from '@mui/system';
-import { AppBar, Box, Button, Paper, Typography } from '@mui/material';
+import { AppBar, Box, Button, Card, CardContent, Grid, Paper, Typography } from '@mui/material';
 
 export default function App() {
   //initial blog setup for useState
@@ -11,19 +11,41 @@ export default function App() {
   //-----------------
   // NAV Buttons
   //-----------------
-  const [isRantsVisible, setIsRantsVisible] = useState(true)
+  const [areRantsVisible, setAreRantsVisible] = useState(true)
   const [isAddARantVisible, setIsAddARantVisible] = useState(false)
 
   const showRants = () => {
-    setIsRantsVisible(true);
+    setAreRantsVisible(true);
     setIsAddARantVisible(false)
 
   }
   const showAddARant = () => {
-    setIsRantsVisible(false);
+    setAreRantsVisible(false);
     setIsAddARantVisible(true)
 
   }
+  // GET requests to API
+  // const getRants = async () => {
+  //   const response = await 
+  //   axios
+  //   .get('http://localhost:3000/blog')
+  //   .catch((err) => {
+  //     console.log(err)
+  //   })
+  //   setRant(response.data.results)
+
+  //   const getAddRant = () => {
+  //     axios
+  //     .get('http://localhost:3000/blog')
+  //     .then((response) => {
+  //       setAddRant(response.data.results);
+  //     })
+  //   }
+  useEffect(() => {
+    axios.get('http://localhost:3000/getpets').then((response) => {
+      setRant(response.data)
+    })
+  }, [])
 
   return (
     <Container>
@@ -35,10 +57,30 @@ export default function App() {
       </Paper>
 
       <Box align="center" sx={{ m: 2, msl: 5 }}>
-        <Button variant={isRantsVisible ? "contained" : "outlined"} color="primary" onClick={showRants} sx={{ mr: 2 }}>Rants</Button>
+        <Button variant={areRantsVisible ? "contained" : "outlined"} color="primary" onClick={showRants} sx={{ mr: 2 }}>Rants</Button>
         <Button variant={isAddARantVisible ? "contained" : "outlined"} color="primary" onClick={showAddARant} sx={{ mr: 2 }}>Add a Rant</Button>
       </Box>
 
+      <Grid container className="rantList">
+        {areRantsVisible
+          && rant.map((character, index) => {
+            return (
+              <Card sx={{ width: { sm: 600 }, m: 1 }} elevation={7} key={rant.id}>
+                {/* <CardMedia sx={{ height: 130 }}
+                    component="img"
+                    image={rant.image}
+                    height="140">
+                  </CardMedia> */}
+                <CardContent>
+                  <Typography>Title: {rant.title}</Typography>
+                  <Typography>Date Written: {rant.date}</Typography>
+                  <Typography>Rant: {rant.content}</Typography>
+                </CardContent>
+              </Card>
+            )
+          }
+          )}
+      </Grid>
 
 
 
